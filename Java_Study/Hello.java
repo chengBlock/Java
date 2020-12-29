5314ba3c39433d0e6fb448612d1f68037d06989e
clcheng <l499886834@163.com>
2020-12-29 15:14:00 +0800

    hello

---
+++ Java_Study/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    /**
+     * 这种注释常用定义文档
+     * 
+     * @author clcheng
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
